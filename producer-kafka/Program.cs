4488c7fb08d0cc2ac500e93da7198f4c509fb4d9
Jonathan Gonzalez Jimenez <jonathan.gonzalezj@tvazteca.com.mx>
2023-06-19 17:15:22 -0600

    Agregar archivos de proyecto.

---
+++ producer-kafka/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using producer_kafka;
+
+var text = File.ReadAllText("data.json");
+List list = JsonConvert.DeserializeObject<List>(text);
+
+const string topic = "Actions";
+var id_producer = $"Worker {new Random().Next(1, 100)}";
+var config = new ProducerConfig()
+{
+    BootstrapServers = "172.176.196.216:9094"
+};
+try
+{
+    using (var producer = new ProducerBuilder<string, string>(config).Build())
+    {
+        foreach (var item in list.objects)
+        {
+            var json = JsonConvert.SerializeObject(item);
+            await producer.ProduceAsync(topic, new Message<string, string>
+            {
+                Key = id_producer,
+                Value = json
+            });
+        }
+    }
+}
+catch (ProduceException<string, string> e)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(e.Message);
+}
---
+++ producer-kafka/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace producer_kafka
+{
+    public class Unit
+    {
+        public int id { get; set; }
+        public string job_title { get; set; }
+        public string email { get; set; }
+        public string fullname { get; set; }
+    }
+}
